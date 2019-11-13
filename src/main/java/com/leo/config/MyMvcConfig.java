bb04e92e235dd6caba16b469b17462783569804a
leo <953086648@qq.com>
2019-11-13 09:33:52 +0800

    commit

--- src/main/java/com/leo/config/MyMvcConfig.java
+++ src/main/java/com/leo/config/MyMvcConfig.java
@@ -1,5 +1,6 @@
 package com.leo.config;
 
+import com.leo.component.MylocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -32,5 +33,10 @@ public class MyMvcConfig implements WebMvcConfigurer {
             }
         };
         return adapter;
+    }
+
+    @Bean
+    public MylocaleResolver localeResolver(){
+        return new MylocaleResolver();
     }
 }
